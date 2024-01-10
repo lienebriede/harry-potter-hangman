6c6e848a1d1eff37eb4348f9ecde50ad04596e8f
Liene Briede <lienesjaunkundze@gmail.com>
2024-01-10 15:02:27 +0000

    Add game code: word list, random choice word, while loop to guess the word

--- run.py
+++ run.py
@@ -1,3 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+wordList = ["unicorn", "phoenix"]
+triedLetters =[]
+
+#takes a random word from the list
+word = random.choice(wordList)
+
+#prints the word
+for i in word:
+    print("_", end="")
+
+print()
+print()
+
+while True:
+    letter = input("Guess a letter > ").lower()
+    
+    if letter in triedLetters:
+        print("You tried that one already!")
+        continue
+
+    #adds picked letters to the list    
+    triedLetters.append(letter)
+
+    if letter in word:
+        print("Yep! It's in there!")
+    else:
+        print("Nope, not in there!")
+
+    #need this boolean to check if word is guessed and break out of the loop    
+    gotAllLetters = True    
+    
+    #prints the guessed letters that are in the word
+    for i in word:
+        if i in triedLetters:
+            print(i, end="")
+        else:
+            print("_", end="")
+            gotAllLetters = False
+
+    #breaks out of the loop
+    if gotAllLetters:
+        print("You guessed the word!")
+        break
+    
+
+
